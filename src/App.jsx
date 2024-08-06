cb801b44d68a99c0a09dae7fbec6cc12e06ea93f
unknown <mo.mahrous80@gmail.com>
2024-08-06 21:13:00 +0300

    add tailwind and avatar with text

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { Avatar, Box, Typography } from "@mui/material";
 import "./App.css";
 
 function App() {
     return (
         <>
-            <h1>Hello World</h1>
+            <Box className="container flex justify-center items-center h-lvh gap-16 mx-auto ">
+                <Avatar
+                    alt="Travis Howard"
+                    src="/images/2.jpg"
+                    sx={{ width: 300, height: 300 }}
+                />
+                <Box sx={{ width: "100%", maxWidth: 600 }}>
+                    <Typography variant="h2" gutterBottom>
+                        Hi, It`s{" "}
+                        <Typography variant="span">Mohamed 👋</Typography>
+                    </Typography>
+                    <Typography variant="h3" gutterBottom>
+                        Software Developer
+                    </Typography>
+                    <Typography variant="h3" color={"Highlight"}>
+                        Frontend and cross platform.
+                    </Typography>
+                </Box>
+            </Box>
         </>
     );
 }
